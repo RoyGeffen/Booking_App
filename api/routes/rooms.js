f4c813dfafe15e0c747f773d310218d80c0017cc
Roy Geffen <roygeffenversano1@gmail.com>
2023-07-19 20:19:45 +0300

    rooms

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,10 +1,17 @@
 import express from "express";
+import{createRoom,deleteRoom,getRoom,getRooms,updateRoom,updateRoomAvailability} from "../controllers/roomControllers.js";
+import { verifyAdmin } from "../middleware/verifyToken.js";
 
-const router  = express.Router();
+const router = express.Router();
 
-router.get("/", (req,res)=>{
-    res.send("ggggggggggggggg");
-})
 
+router.post("/:hotelid", verifyAdmin, createRoom);
+//UPDATE
+router.put("/availability/:id", updateRoomAvailability);
+router.put("/:id", verifyAdmin, updateRoom);
+
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+router.get("/:id", getRoom);
+router.get("/", getRooms);
 
 export default router;
