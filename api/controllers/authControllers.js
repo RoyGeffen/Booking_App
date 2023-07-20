65a155b388e8fcb68d45d94634933fbae914fd35
Roy Geffen <roygeffenversano1@gmail.com>
2023-07-20 20:51:01 +0300

    day 2

--- api/controllers/authControllers.js
+++ api/controllers/authControllers.js
@@ -51,11 +51,21 @@ export const login = asyncHandler(async (req,res,next)=>{
             }).status(200)
               .json({title:"authorized",
                 success:true,
-                "username":username,
-                "email":user.email
+                "user":user
             });
     }else{
         res.status(401)
         throw new Error("Wrong password or email");
     } 
 });
+
+export const logout = async (req,res,next)=>{
+    // Set token to none and expire after 5 seconds
+    res.cookie('access_token', 'none', {
+        expires: new Date(Date.now() + 5 * 1000),
+        httpOnly: true,
+    })
+    res
+        .status(200)
+        .json({ success: true, message: 'User logged out successfully' })
+}
--- front/src/components/navbar/Navbar.jsx
+++ front/src/components/navbar/Navbar.jsx
@@ -1,16 +1,45 @@
+import { useContext } from "react"
+import { AuthContext } from "../../context/AuthContext"
 import "./navbar.css"
-import {Link} from "react-router-dom"
+import {Link, Navigate, useNavigate} from "react-router-dom"
+import axios from "axios"
 const Navbar = () => {
+
+  const {user, dispatch} = useContext(AuthContext)
+  const navigate = useNavigate()
+
+  const LogOutUser = async (e) => {
+    try {
+        await axios.post('/auth/logout')
+        localStorage.removeItem('user')
+        dispatch({ type:"USER_LOGOUT" })
+        window.location.reload(); // weird
+        //navigate("/")
+    } catch (error) {
+        console.log(error)
+      }
+  }
+
+  const LoginClick = ()=>{
+    navigate("/login")
+  }
+
+
   return (
     <div className="navbar">
       <div className="navContainer">
         <Link to ="/" style={{color:"inherit", textDecoration:"none"}}>
           <span className="logo">RoyBooking</span>
         </Link>
-        <div className="navItems">
-          <button className="navButton">Register</button>
-          <button className="navButton">Login</button>
-        </div>
+        {user ? 
+            (<div>
+              {user.username}
+              <button className="logout-button" onClick={LogOutUser}>Log Out</button>
+            </div>)
+          : (<div className="navItems">
+              <button className="navButton">Register</button>
+              <button className="navButton" onClick={LoginClick}>Login</button>
+            </div>)}
       </div>
     </div>
   )
